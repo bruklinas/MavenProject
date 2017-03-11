8d506314a62fe1efa1d274d51ec19c14d84786b9
bruklinas <bruklinas@gmail.com>
2017-03-11 10:31:52 +0200

    Test commit. Second sysout added.

--- src/main/java/MVN/Test/App.java
+++ src/main/java/MVN/Test/App.java
@@ -4,10 +4,9 @@ package MVN.Test;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Updated.");
+	}
 }
